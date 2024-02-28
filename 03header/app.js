41d4da19abb92827d27cd575cc106a59782593fd
Dipak Sinha <dipaksinha2022@gmail.com>
2024-02-28 10:53:20 +0545

    show mobile menu while toggling is made

--- 03header/app.js
+++ 03header/app.js
@@ -1,5 +1,15 @@
 const toggleMenu = document.querySelector(".toggle-container");
-
 toggleMenu.addEventListener("click", function () {
   toggleMenu.classList.toggle("change");
+  showMobileMenu();
 });
+
+// for mobile menu
+const ulList = document.querySelector(".list-menu");
+function showMobileMenu() {
+  if (ulList.style.display === "block") {
+    ulList.style.display = "none";
+  } else {
+    ulList.style.display = "block";
+  }
+}
