a5375f4c37403cedccfce0d0ea4966455cf12d21
Dipak Sinha <dipaksinha2022@gmail.com>
2024-02-29 12:47:12 +0545

    this is okay but it will only targetting the first questiona answer

--- 101Accordion/app.js
+++ 101Accordion/app.js
@@ -1,2 +1,9 @@
-
-
+const queBox = document.querySelector(".question-box");
+const ansBox = document.querySelector(".answer-box");
+queBox.addEventListener("click", function () {
+  if (ansBox.style.display === "block") {
+    ansBox.style.display = "none";
+  } else {
+    ansBox.style.display = "block";
+  }
+});
