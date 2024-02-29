fe6ac5453f98f9c1461293b130a4133a535d293f
Dipak Sinha <dipaksinha2022@gmail.com>
2024-02-29 12:48:00 +0545

    this is good practise to make accordion by targeting all questions

--- 101Accordion/app.js
+++ 101Accordion/app.js
@@ -1,9 +1,25 @@
-const queBox = document.querySelector(".question-box");
-const ansBox = document.querySelector(".answer-box");
-queBox.addEventListener("click", function () {
-  if (ansBox.style.display === "block") {
-    ansBox.style.display = "none";
-  } else {
-    ansBox.style.display = "block";
-  }
-});
+var queBox = document.getElementsByClassName("question-box");
+// const ansBox = document.querySelector(".answer-box"); // not required
+var i;
+for (i = 0; i < queBox.length; i++) {
+  queBox[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    // this ansBox doesn't target anything it is just targeting the nextElementSibling
+    var ansBox = this.nextElementSibling;
+    if (ansBox.style.display === "block") {
+      ansBox.style.display = "none";
+    } else {
+      ansBox.style.display = "block";
+    }
+  });
+}
+
+// const queBox = document.querySelector(".question-box");
+// const ansBox = document.querySelector(".answer-box");
+// queBox.addEventListener("click", function () {
+//   if (ansBox.style.display === "block") {
+//     ansBox.style.display = "none";
+//   } else {
+//     ansBox.style.display = "block";
+//   }
+// });
